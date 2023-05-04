45e7454d4a782b5fe1d30e49a8d87499a318fc77
Therock90421 <lihaochen17@mails.ucas.ac.cn>
2023-05-04 10:05:39 -0400

    Edit output dir

--- test_da_clip_k2c.sh
+++ test_da_clip_k2c.sh
@@ -5,7 +5,7 @@ python3 ./tools/train_net.py \
 --eval-only  \
 --num-gpus 1 \
 --config-file ./configs/PascalVOC-Detection/da_clip_faster_rcnn_R_50_C4_k2c.yaml \
-MODEL.WEIGHTS ./output/model_ps_ent_withattn_55.9mAP.pth \
+MODEL.WEIGHTS ./pretrained_ckpt/regionclip/regionclip_finetuned-coco_rn50.pth \
 MODEL.CLIP.OFFLINE_RPN_CONFIG ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml \
 MODEL.CLIP.BB_RPN_WEIGHTS ./pretrained_ckpt/rpn/rpn_coco_48.pth \
 MODEL.CLIP.TEXT_EMB_PATH ./pretrained_ckpt/concept_emb/cityscapes_car_emb.pth \
